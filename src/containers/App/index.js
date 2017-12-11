a97f6be91def9a01ebfd97f23f703b3b1478cb56
murgovska <murgovskakristina@gmail.com>
2017-12-11 21:56:34 +0100

    Created actions, reducers, containers (App), reducer directories

---
+++ src/containers/App/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../../helpers';
+import { alertActions } from '../../actions';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const { dispatch } = this.props;
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+            <div className="container-fluid">
+            App
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+export { connectedApp as App }; 
